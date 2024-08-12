0e3961d0bf14a2ad56667edbbc9dd01d8201b045
冯思琳 <2822132073@qq.com>
2024-08-13 00:13:09 +0800

    update data model

--- djTest01/settings.py
+++ djTest01/settings.py
@@ -76,13 +76,25 @@ WSGI_APPLICATION = 'djTest01.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'djTest01',
+        'USER': 'root',
+        'PASSWORD': 'Fsl2000.',
+        'HOST': '106.75.231.65',
+        'PORT': '3306',
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+        },
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
